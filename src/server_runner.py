e3c6ee84f15bdf1a2e82d59b52a31198cde76c2b
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-14 00:17:18 +0900

    complete running version

--- src/server_runner.py
+++ src/server_runner.py
@@ -1,18 +1,23 @@
 # quick and dirty solution fix later
-import sys  # noqa
-from os import environ, path  # noqa
+import sys
+from os import environ, path
 
 current_dir = path.dirname(path.abspath(__file__))  # noqa
-sys.path.append(path.join(current_dir, '../MQTT-image-database-server/src'))  # noqa
+parent_dir = path.dirname(current_dir)  # noqa
+sys.path.append(path.join(parent_dir, 'MQTT-image-database-server/src'))  # noqa
+sys.path.append(path.join(parent_dir, 'image-management-server/src'))  # noqa
 
 import json
-from os import environ
+import uvicorn
+import subprocess
+
 from image_database_server import ImageDataBaseServer
 
 
 connection_string = environ.get('TEST_DATABASE_CONNECTION_STRING')
 
-config = '{ \
+config = json.loads(
+    '{ \
             "mqtt_subscriber":{ \
                 "broker_url": "broker.hivemq.com", \
                 "broker_port": 1883, \
@@ -23,13 +28,29 @@ config = '{ \
                 "database": "test", \
                 "collection": "test" \
             } \
-        }'
+        }')
 
 
-json_config = json.loads(config)
-json_config['mongo_handler']['connection_string'] = connection_string
-img_database_server = ImageDataBaseServer(json_config)
+sys.argv = ['_', connection_string, config['mongo_handler']
+            ['database'], config['mongo_handler']['collection']]
+
+from image_management_server import app  # noqa
+
+
+try:
+    print(
+        path.join(parent_dir, 'image_management_ui'))
+    subprocess.Popen(['npm', 'run', 'start'], start_new_session=True, shell=True, cwd=path.join(
+        parent_dir, 'image_management_ui'))
+except subprocess.CalledProcessError as e:
+    print(f"Error starting npm server: {e}")
+
+config['mongo_handler']['connection_string'] = connection_string
+img_database_server = ImageDataBaseServer(config)
 img_database_server.run()
+uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# todo: kill processes gracefully
 while True:
     try:
         pass
